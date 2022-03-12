7f1fafb8c3cdfd2c32c927b27832a8851ea4ffb0
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-12 20:17:30 -0300

    Add result component and user nfts components

---
+++ ui/src/lib/Result.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { winner, prize, flagName, flagImage } from '../../../shared/store/store';
+</script>
+
+<div class="w3-container w3-center">
+	{#if $winner}
+		<h1>You Won!</h1>
+		<p>Well done you have won {$prize} XLM and the {$flagName}</p>
+		<img src={$flagImage} alt={$flagName} />
+	{:else}
+		<h1>You lost!</h1>
+		<p>Try again next round</p>
+	{/if}
+</div>
--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Result from '../lib/Result.svelte';
+	import { winner, prize, flagName, flagImage, userNFTs } from '../../../shared/store/store';
+	import UserAssets from '../lib/UserAssets.svelte';
+	import ARGENTINIAN_FLAG from '../../../shared/flags/270x180/arg.png';
+	import BOLIVIAN_FLAG from '../../../shared/flags/270x180/bol.png';
+	import BRAZIL_FLAG from '../../../shared/flags/270x180/bra.png';
+	import GMB_FLAG from '../../../shared/flags/270x180/gmb.png';
+
+	$userNFTs = [ARGENTINIAN_FLAG, BOLIVIAN_FLAG, BRAZIL_FLAG, GMB_FLAG];
+	$winner = true;
+	$prize = '100';
+	$flagName = 'ARGENTINIAN FLAG NFT';
+	$flagImage = ARGENTINIAN_FLAG;
+</script>
+
+<Result />
+<UserAssets />
