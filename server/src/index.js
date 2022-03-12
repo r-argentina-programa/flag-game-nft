29b30206ddc4b1ae2fabfbf3137ce3883ecb556b
Leonel Gauna <leonel.mariano.g@gmail.com>
2022-03-11 22:09:04 -0300

    Update socket.io implementation

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,23 @@
-const express = require("express");
+require('dotenv').config();
+
+const express = require('express');
 const app = express();
-const http = require("http");
+const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-const { SECONDS_PER_MESSAGE } = require("./constants.js");
-const path = require('path');
+const { Server } = require('socket.io');
+const io = new Server(server, {
+	cors: {
+		origin: process.env.FRONTEND_URL
+	}
+});
+const { SECONDS_PER_MESSAGE } = require('./constants.js');
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'src/app.html'))
+io.on('connection', () => {
+	console.log('a user connected');
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-
-setInterval(
-  () => io.emit("message", "This is supposed to be a pixel"),
-  SECONDS_PER_MESSAGE
-);
+setInterval(() => io.emit('message', 'This is supposed to be a pixel'), SECONDS_PER_MESSAGE);
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}....`));
