d1ba992ed91bafeb027a31b5dca42f931a1c5792
Ja-boop <56199168+Ja-boop@users.noreply.github.com>
2022-03-13 00:41:17 -0300

    Delete duplicate store and empty component

--- shared/store/store.js
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export const winner = writable(false);
-export const prize = writable("");
-export const flagName = writable("");
-export const flagImage = writable("");
-export const userNFTs = writable([]);
