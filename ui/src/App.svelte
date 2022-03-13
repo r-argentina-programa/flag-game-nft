bdbd5081231b1f5507f5c0940ab8adb04642c2e2
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2022-03-13 19:59:07 +1100

    Revert "Add feature to connect with private key"

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,14 +1,8 @@
-<script>
+<script lang="ts">
 	import { io } from 'socket.io-client';
 	import Rounds from './ui/lib/Rounds.svelte';
-	import Connect from './ui/lib/Connect.svelte';
-	import { isConnected } from './ui/store/store';
 	const socket = io(import.meta.env.VITE_SERVER_URL);
 	socket.on('message', (data) => console.log(data));
 </script>
 
-{#if $isConnected}
-	<Rounds />
-{:else}
-	<Connect />
-{/if}
+<Rounds />
--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,13 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// kit: {
-	// 	appDir: './ui',
-	// 	adapter: adapter()
-	// }
-	preprocess: preprocess(),
 	kit: {
 		appDir: './ui',
 		adapter: adapter()
