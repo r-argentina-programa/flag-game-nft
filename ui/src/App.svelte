1c6d6e4c84e02a46f7088799ccecdd1cb32c5139
Genebson <mauryavero@gmail.com>
2022-03-13 03:10:04 -0300

    Add connection with private key

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,8 +1,14 @@
-<script lang="ts">
+<script>
 	import { io } from 'socket.io-client';
 	import Rounds from './ui/lib/Rounds.svelte';
+	import Connect from './ui/lib/Connect.svelte';
+	import { isConnected } from './ui/store/store';
 	const socket = io(import.meta.env.VITE_SERVER_URL);
 	socket.on('message', (data) => console.log(data));
 </script>
 
-<Rounds />
+{#if $isConnected}
+	<Rounds />
+{:else}
+	<Connect />
+{/if}
--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,7 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// kit: {
+	// 	appDir: './ui',
+	// 	adapter: adapter()
+	// }
+	preprocess: preprocess(),
 	kit: {
 		appDir: './ui',
 		adapter: adapter()
